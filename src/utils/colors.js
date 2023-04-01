b45abc968fd06f7daf73559e1e598242d72c4770
Stas <simplepage96@gmail.com>
2023-04-01 23:23:02 +0600

    clean up template

--- src/utils/colors.js
+++
@@ -1,8 +0,0 @@
-export const randomizeColor = () => Math.floor(Math.random() * 1000000);
-
-export const invertHex = (hex) => {
-	return (Number(`0x1${hex}`) ^ 0xffffff)
-		.toString(16)
-		.substring(1)
-		.toUpperCase();
-};
